c5155dfc2440968d77e7a6ebbe8e171e40c62ba7
SolidSquid <davide0695es@gmail.com>
2021-12-21 00:37:31 +0200

    Tuple Tets

---
+++ RayTracer.Test/TupleTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RayTracer.Test
+{
+    class TupleTest
+    {
+        [Test]
+        public void TupleAdditionTest()
+        {
+            var tuples = new List<Tuple>() { new Tuple { X = 1, Y = 2, Z = 3, W = 1, }, new Tuple { X = 1, Y = 2, Z = 3, W = 0 } };
+
+            // Möglichkeit 1 mit LINQ
+            // List<Point> points = tuples.Where(t => t != null && t.W == 1).Select(t => Point.MapTupleToPoint(t)).ToList();
+
+            // Möglichkeit 2 mit foreach
+            List<Point> points2 = new List<Point>();
+
+            foreach(Tuple t in tuples)
+            {
+                if(t != null && t.W == 1)
+                {
+                    points2.Add(Point.MapTupleToPoint(t));
+                }
+            }
+
+        }
+    }
+}
